a723b89a1a18e8564b5aa12bd58ffded71f91ed5
smyeon <smyeon@globalsoft.co.kr>
2017-09-27 14:51:46 +0900

    reorganize to create-react-app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import 'semantic-ui-css/semantic.min.css';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
