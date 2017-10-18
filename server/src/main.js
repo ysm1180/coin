ecd75e67deeeb0559eef90a709681eac538c1dfa
smyeon <smyeon@globalsoft.co.kr>
2017-10-18 13:19:07 +0900

    Modify server for production

--- server/src/main.js
+++ server/src/main.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import api from './routes';
+import path from 'path';
 
 const app = express();
 
+app.set('views', path.join(__dirname, '..', '..', 'build'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '..', '..', 'build')));
+
 app.use('/api', api);
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
 
 app.listen(3000);
